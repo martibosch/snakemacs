ccf720ae37b48b5a2962108f663338eeea2c9b0e
Martí Bosch <marti.bosch-padros@inria.fr>
2016-05-02 15:02:48 +0200

    changed melpa repo uri

--- my-packages.el
+++ my-packages.el
@@ -1,6 +1,6 @@
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
+             '("melpa" . "http://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
