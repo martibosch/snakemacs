00b7214964a219501c5a61c498b03101ead4829c
Martí Bosch <marti.bosch-padros@inria.fr>
2016-05-02 14:44:48 +0200

    changed melpa repo uri

--- my-packages.el
+++ my-packages.el
@@ -1,6 +1,6 @@
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
