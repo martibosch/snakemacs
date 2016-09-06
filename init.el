2779e69a571c4cafcffabe7c08b2e0a190f1394b
Martí Bosch <marti.bosch.1992@gmail.com>
2016-09-06 15:41:09 +0200

    added Conkeror default browsing

--- init.el
+++ init.el
@@ -97,3 +97,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; conkeror
+
+(if (executable-find "conkeror")
+    (progn (setq browse-url-generic-program (executable-find "conkeror"))
+           (setq browse-url-browser-function 'browse-url-generic)))
