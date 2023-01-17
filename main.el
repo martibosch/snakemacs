0894ec054f25539e4a0bc027c79d3d0d261e9abe
Martí Bosch <marti.bosch@protonmail.com>
2023-01-17 08:16:55 +0100

    feat: changed flake8 for ruff

--- main.el
+++ main.el
@@ -71,6 +71,29 @@
 
 ;; syntax checker
 (use-package flycheck
+  :config
+  (flycheck-define-checker python-ruff
+    "A Python syntax and style checker using the ruff utility.
+    To override the path to the ruff executable, set
+    `flycheck-python-ruff-executable'.
+    See URL `http://pypi.python.org/pypi/ruff'."
+    :command ("ruff"
+              "--format=text"
+              (eval (when buffer-file-name
+                      (concat "--stdin-filename=" buffer-file-name)))
+              "-")
+    :standard-input t
+    :error-filter (lambda (errors)
+                    (let ((errors (flycheck-sanitize-errors errors)))
+                      (seq-map #'flycheck-flake8-fix-error-level errors)))
+    :error-patterns
+    ((warning line-start
+              (file-name) ":" line ":" (optional column ":") " "
+              (id (one-or-more (any alpha)) (one-or-more digit)) " "
+              (message (one-or-more not-newline))
+              line-end))
+    :modes python-mode)
+  (add-to-list 'flycheck-checkers 'python-ruff)
   :init (global-flycheck-mode))
 
 ;; code parsing
