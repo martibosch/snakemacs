18e6b085152f633f4d0f74c4ff1a1dad9bba1883
Martí Bosch <marti.bosch@protonmail.com>
2023-02-21 10:45:15 +0100

    feat: added copilot

--- main.el
+++ main.el
@@ -77,6 +77,14 @@
   company
   :config (global-company-mode))
 
+(use-package copilot
+  :straight (:host github :repo "zerolfx/copilot.el" :files ("dist" "*.el"))
+  :after company
+  :config (delq 'company-preview-if-just-one-frontend company-frontends)
+  (define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
+  (define-key copilot-completion-map (kbd "TAB") 'copilot-accept-completion)
+  :hook ((prog-mode . (lambda () (unless (equal (buffer-name) "*scratch*") copilot-mode)))))
+
 ;; syntax checker
 (use-package flycheck
   :config
