1d26d757b534598ca02e8fb71f2b285f61223fb1
Martí Bosch <marti.bosch@protonmail.com>
2023-05-15 18:02:20 +0200

    feat: add jupyter my/ functions from sqrtminusone

--- main.el
+++ main.el
@@ -86,4 +86,57 @@
 							   (shell . t)
 							   (jupyter . t)))
   :hook
-  (org-babel-after-execute . org-display-inline-images)))
+  (org-babel-after-execute . org-display-inline-images))
+
+(defun my/get-open-ports ()
+  (mapcar
+   #'string-to-number
+   (split-string (shell-command-to-string "ss -tulpnH | awk '{print $5}' | sed -e 's/.*://'") "\n")))
+
+(setq my/jupyter-runtime-folder (expand-file-name "~/.local/share/jupyter/runtime"))
+
+(defun my/list-jupyter-kernel-files ()
+  (mapcar
+   (lambda (file) (cons (car file) (cdr (assq 'shell_port (json-read-file (car file))))))
+   (sort
+    (directory-files-and-attributes my/jupyter-runtime-folder t ".*kernel.*json$")
+    (lambda (x y) (not (time-less-p (nth 6 x) (nth 6 y)))))))
+
+(defun my/select-jupyter-kernel ()
+  (let ((ports (my/get-open-ports))
+        (files (my/list-jupyter-kernel-files)))
+    (completing-read
+     "Jupyter kernels: "
+     (seq-filter
+      (lambda (file)
+        (member (cdr file) ports))
+      files))))
+
+(defun my/insert-jupyter-kernel ()
+  "Insert a path to an active Jupyter kernel into the buffer"
+  (interactive)
+  (insert (my/select-jupyter-kernel)))
+
+(defun my/jupyter-connect-repl ()
+  "Open emacs-jupyter REPL, connected to a Jupyter kernel"
+  (interactive)
+  (jupyter-connect-repl (my/select-jupyter-kernel) nil nil nil t))
+
+(defun my/jupyter-qtconsole ()
+  "Open Jupyter QtConsole, connected to a Jupyter kernel"
+  (interactive)
+  (start-process "jupyter-qtconsole" nil "setsid" "jupyter" "qtconsole" "--existing"
+                 (file-name-nondirectory (my/select-jupyter-kernel))))
+
+(defun my/jupyter-cleanup-kernels ()
+  (interactive)
+  (let* ((ports (my/get-open-ports))
+         (files (my/list-jupyter-kernel-files))
+         (to-delete (seq-filter
+                     (lambda (file)
+                       (not (member (cdr file) ports)))
+                     files)))
+    (when (and (length> to-delete 0)
+               (y-or-n-p (format "Delete %d files?" (length to-delete))))
+      (dolist (file to-delete)
+        (delete-file (car file)))))
