22af47ba1aa261da730062149555e628e5729b25
Martí Bosch <marti.bosch@protonmail.com>
2023-08-07 16:16:24 +0200

    fix: dropped config workarounds for conda-forge emacs

--- early-init.el
+++ early-init.el
@@ -5,6 +5,6 @@
 (setq package-enable-at-startup nil)
 
 ;; https://github.com/conda-forge/emacs-feedstock/issues/60
-(if (eq system-type 'gnu/linux)
-    (progn (setq configure-info-directory (string-replace "\0" "" configure-info-directory))
-	   (setf (car Info-default-directory-list) configure-info-directory)))
+;; (if (eq system-type 'gnu/linux)
+;;     (progn (setq configure-info-directory (string-replace "\0" "" configure-info-directory))
+;; 	   (setf (car Info-default-directory-list) configure-info-directory)))
