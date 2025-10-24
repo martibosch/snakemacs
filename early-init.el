eb18cef29830c9ee643913ae5cbbf4143b11c591
Martí Bosch <marti.bosch@protonmail.com>
2025-10-24 15:58:22 +0200

    feat: lsp booster draft

--- early-init.el
+++ early-init.el
@@ -8,3 +8,6 @@
 ;; (if (eq system-type 'gnu/linux)
 ;;     (progn (setq configure-info-directory (string-replace "\0" "" configure-info-directory))
 ;; 	   (setf (car Info-default-directory-list) configure-info-directory)))
+
+;; for lsp-booster
+(setenv "LSP_USE_PLISTS" "true")
--- main.el
+++ main.el
@@ -325,7 +325,72 @@ See URL `https://docs.astral.sh/ruff/'."
        (lsp-deferred))))
   :config
   (setq lsp-pylsp-plugins-ruff-enabled t)
-  :commands lsp lsp-deferred)
+  :commands lsp lsp-deferred
+  ;; https://www.ovistoica.com/blog/2024-7-05-modern-emacs-typescript-web-tsx-config#orgc88562e
+  ;; https://github.com/blahgeek/emacs-lsp-booster/issues/19
+  ;; :preface
+  ;; (defun lsp-booster--advice-json-parse (old-fn &rest args)
+  ;;   "Try to parse bytecode instead of json."
+  ;;   (or
+  ;;    (when (equal (following-char) ?#)
+
+  ;;      (let ((bytecode (read (current-buffer))))
+  ;;        (when (byte-code-function-p bytecode)
+  ;;          (funcall bytecode))))
+  ;;    (apply old-fn args)))
+  ;; (defun lsp-booster--advice-final-command (old-fn cmd &optional test?)
+  ;;   "Prepend emacs-lsp-booster command to lsp CMD."
+  ;;   (let ((orig-result (funcall old-fn cmd test?)))
+  ;;     (if (and (not test?) ;; for check lsp-server-present?
+  ;;              (not (file-remote-p default-directory)) ;; see lsp-resolve-final-command, it would add extra shell wrapper
+  ;;              lsp-use-plists
+  ;;              (not (functionp 'json-rpc-connection)) ;; native json-rpc
+  ;;              (executable-find "emacs-lsp-booster"))
+  ;;         (progn
+  ;;           (message "Using emacs-lsp-booster for %s!" orig-result)
+  ;;           (cons "emacs-lsp-booster" orig-result))
+  ;;       orig-result)))
+  ;; :init
+  ;; (setq lsp-use-plists t)
+  ;; ;; Initiate https://github.com/blahgeek/emacs-lsp-booster for performance
+  ;; (advice-add (if (progn (require 'json)
+  ;;                        (fboundp 'json-parse-buffer))
+  ;;                 'json-parse-buffer
+  ;;               'json-read)
+  ;;             :around
+  ;;             #'lsp-booster--advice-json-parse)
+  ;; (advice-add 'lsp-resolve-final-command :around #'lsp-booster--advice-final-command)
+  )
+(defun lsp-booster--advice-json-parse (old-fn &rest args)
+  "Try to parse bytecode instead of json."
+  (or
+   (when (equal (following-char) ?#)
+     (let ((bytecode (read (current-buffer))))
+       (when (byte-code-function-p bytecode)
+         (funcall bytecode))))
+   (apply old-fn args)))
+(advice-add (if (progn (require 'json)
+                       (fboundp 'json-parse-buffer))
+                'json-parse-buffer
+              'json-read)
+            :around
+            #'lsp-booster--advice-json-parse)
+
+(defun lsp-booster--advice-final-command (old-fn cmd &optional test?)
+  "Prepend emacs-lsp-booster command to lsp CMD."
+  (let ((orig-result (funcall old-fn cmd test?)))
+    (if (and (not test?)                             ;; for check lsp-server-present?
+             (not (file-remote-p default-directory)) ;; see lsp-resolve-final-command, it would add extra shell wrapper
+             lsp-use-plists
+             (not (functionp 'json-rpc-connection))  ;; native json-rpc
+             (executable-find "emacs-lsp-booster"))
+        (progn
+          (when-let ((command-from-exec-path (executable-find (car orig-result))))  ;; resolve command from exec-path (in case not found in $PATH)
+            (setcar orig-result command-from-exec-path))
+          (message "Using emacs-lsp-booster for %s!" orig-result)
+          (cons "emacs-lsp-booster" orig-result))
+      orig-result)))
+(advice-add 'lsp-resolve-final-command :around #'lsp-booster--advice-final-command)
 
 ;; (use-package
 ;;   lsp-pyright
