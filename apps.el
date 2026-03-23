b070a445e25d7f059b6ebdb07a7a5a0ab22a6d2b
Martí Bosch <marti.bosch@protonmail.com>
2026-03-23 11:27:44 +0100

    feat: projectile-based vterm buffer naming

--- apps.el
+++ apps.el
@@ -6,4 +6,15 @@
 (use-package
   vterm
   :custom (vterm-install t)
-  (vterm-always-compile-module t))
+  (vterm-always-compile-module t)
+  :config
+  (defun my/vterm-buffer-name ()
+    (let ((project (projectile-project-name)))
+      (if (string= project "-")
+          "*vterm*"
+        (format "*vterm[%s]*" project))))
+  (add-hook 'vterm-mode-hook
+            (lambda ()
+              ;; only rename generic vterm buffers; leave ai-code-named ones alone
+              (when (string-match-p "\\`\\*vterm" (buffer-name))
+                (rename-buffer (my/vterm-buffer-name) t)))))
